dcde667059f578fc7351fa47c1d6884717c76bc1
rexscript <49195717+Kanishk1764@users.noreply.github.com>
2024-09-14 10:51:07 +0530

    Update app.py

--- Inventory/app.py
+++ Inventory/app.py
@@ -45,4 +45,4 @@ def active_dues():
     return render_template('active_dues.html', dues=dues)
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000,debug=False)
+    app.run(host="localhost", port=8410,debug=False)
